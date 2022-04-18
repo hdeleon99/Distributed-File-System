520e76624a01e2feeed1d4c389e3b132df932705
Issiah Deleon <72473835+hdeleon99@users.noreply.github.com>
2022-04-18 13:36:06 -0700

    Updated Client.java
    Added output and input streams, socket, and loop to prompt user for task to be completed. Added new methods, taskManager, and sendFile.

--- Implementation/Client.java
+++ Implementation/Client.java
@@ -1,11 +1,15 @@
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 public class Client
 {
 	private boolean loggedIn = false;
 	private User user;
 	private static Server server = null; //Static means this variable is shared across all "Client" instances
 	
-	public Client() {}
+//	public Client() {}
 	
 	public void LogIn(User user)
 	{
@@ -29,7 +33,55 @@ public class Client
 		//If the client list is 0 after above operation, close server
 	}
 	
-	public void ReaDFile() {}
+	
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		
+		DataOutputStream dataOutputStream = null;
+		DataInputStream dataInputStream = null;
+		Scanner scanner = new Scanner(System.in);
+		
+		try(Socket socket = new Socket("localhost", 1234)) {
+			dataInputStream = new DataInputStream(socket.getInputStream());
+			dataOutputStream = new DataOutputStream(socket.getOutputStream());
+			
+			// user opens client and wants to send a file
+			while(true) {
+				
+				System.out.print("Would you like to: upload, receive, or exit? ");
+				String userTask = scanner.nextLine();
+				dataOutputStream.writeUTF(userTask);
+				// call taskManager here
+				if(userTask.equalsIgnoreCase("exit")) {
+					break;
+				}
+				
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private static void taskManager(String task) {
+		// if task == upload
+		// code to upload (send) a file to the server and DFS system
+		// file will need to be sent to server
+		// file will need to be added to storage
+		// if task == receive
+		// code to ask server for a file
+	}
+	// Function to send the file from the client, to the server
+	private static void sendFile(String path) throws Exception{
+		int bytes = 0;
+		File file;
+		// will need to get file name
+		
+		FileInputStream fileInputStream = new FileInputStream(file);
+		
+		
+		
+	}
+	public void ReadFile() {}
 	public File CreateFile() {}
 	public void WriteToFile(File file) {}
 	public void AddFileToServer(File file) {}
