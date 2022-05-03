1b0b012e2d02e206d61fa440decf00f83b4343fb
Issiah Deleon <72473835+hdeleon99@users.noreply.github.com>
2022-05-03 11:14:54 -0700

    Add files via upload

--- Implementation/File.java
+++ Implementation/File.java
@@ -1,27 +1,19 @@
+package mainpackage;
+import java.io.Serializable;
 
-public class File
+public class File implements Serializable
 {
 	private String name;
-	private String path;
-	private float size;
 	private String type;
 	
-	public File(String name, String path, float size, String type)
+	public File(String name, String type)
 	{
 		this.name = name;
-		this.path = path;
-		this.size = size;
 		this.type = type;
 	}
 	
 	public String GetName() {return name;}
 	public void SetName(String name) {this.name = name;}
-	
-	public String GetPath() {return path;}
-	public void SetPath(String path) {this.path = path;}
-	
-	public float GetSize() {return size;}
-	public void SetSize(float size) {this.size = size;}
 	
 	public String GetType() {return type;}
 	public void SetType(String type) {this.type = type;}
