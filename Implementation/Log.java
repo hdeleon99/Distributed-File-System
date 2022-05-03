d38d3179c92a5e1f0c7b5d25764b029e4e7745e9
Issiah Deleon <72473835+hdeleon99@users.noreply.github.com>
2022-05-03 11:15:37 -0700

    Add files via upload

--- Implementation/Log.java
+++ Implementation/Log.java
@@ -1,12 +1,29 @@
+package mainpackage;
+import java.io.Serializable;
 import java.util.*;
 
-public class Log
+public class Log implements Serializable
 {
-	private String[] actions = new String[7];
-	private Date[] dates = new Date[7];
+	private List<String> actions = new ArrayList<String>();
+	private List<Date> dates = new ArrayList<Date>();
 	
 	public Log() {}
 	
-	public void AppendLog(String action, Date date) {}
-	public void ClearLog() {}
+	public void AppendLog(String action, Date date) {
+		actions.add(action);
+		dates.add(date);
+	}
+	public void ClearLog() {
+		actions.clear();
+		dates.clear();
+	}
+	
+	public String printLog() {
+		String str = "";
+		for(int i = 0; i < actions.size(); i++) {
+			
+			str += (actions.get(i) + "\nDate: " + String.valueOf(dates.get(i)));
+		}
+		return str;
+	}
 }
