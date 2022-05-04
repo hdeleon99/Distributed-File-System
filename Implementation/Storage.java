41ee88f73fce6ed1e6ab038994078d738430cf7a
Issiah Deleon <72473835+hdeleon99@users.noreply.github.com>
2022-05-04 14:02:06 -0700

    Add files via upload

--- Implementation/Storage.java
+++ Implementation/Storage.java
@@ -1,10 +1,9 @@
 package mainpackage;
+
 public interface Storage
 {
-	public File[] GetFileList();
 	public void AppendFileList(File file);
 	public void DeleteFromFileList(String fileName, String fileType);
 	public boolean FileListContains(File file);
 	public File GetFile(String name, String type);
-	public int GetNumFiles();
 }
